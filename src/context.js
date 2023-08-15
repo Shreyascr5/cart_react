bafe0f27f4887eb4cb57543141d39f97069df070
Shreyascr5 <shreyas050801@gmail.com>
2023-08-15 17:37:38 +0530

    finished the main operation for total and cartitem

--- src/context.js
+++ src/context.js
@@ -14,11 +14,32 @@ const initialState={
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] =useReducer(reducer,initialState)
+const clearCart=()=>{
+  dispatch({type:'CLEAR_CART'});
+}
+const remove=(id)=>{
+  dispatch({type:'REMOVE',payload:id})
+}
+const increase=(id)=>{
+  dispatch({type:"INCREASE",payload:id})
+}
+const decrease=(id)=>{
+  dispatch({type:"DECREASE",payload:id})
+}
+useEffect(()=>{
+ dispatch({type:"GET_TOTAL"})
+},[state.cart]);
+
 
   return (
     <AppContext.Provider
       value={{
         ...state ,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+
       }}
     >
       {children}
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,55 @@
+import CartItem from "./CartItem";
+
 const reducer=(state,action)=>{
+    if(action.type==='CLEAR_CART'){
+        return {...state,cart:[]}
+    }
+    if(action.type==="REMOVE"){
+        return{...state,cart:state.cart.filter((CartItem)=>
+            CartItem.id!==action.payload
+        )}
+    }
+    if(action.type==="INCREASE"){
+       let tempCart=state.cart.map((CartItem)=>{
+        if(CartItem.id===action.payload){
+            return{...CartItem,amount:CartItem.amount+1}
+        }
+        return CartItem;
+       });
+       return{...state,cart:tempCart}
+    }
+    if(action.type==="DECREASE"){
+        let tempCart=state.cart.map((CartItem)=>{
+         if(CartItem.id===action.payload){
+             return{...CartItem,amount:CartItem.amount-1}
+         }
+         return CartItem;
+        }).filter((CartItem)=>CartItem.amount!==0);
+        return{...state,cart:tempCart}
+     }
+     if(action.type==="GET_TOTAL"){
+        let {total , amount}=state.cart.reduce(
+            (cartTotal,cartItem)=>{
+                const{price,amount}=cartItem;
+                console.log(price,amount);
+
+                const itemTotal=price*amount;
+                cartTotal.total+=itemTotal;
+
+                cartTotal.amount+=amount;
+                return cartTotal;
+            },{
+                total:0,
+                amount:0
+            }
+        )
+        total=parseFloat(total.toFixed(2));
+        return{...state,total,amount}
+     }
+        
+
+    
     return state;
 }
+
 export default reducer;
