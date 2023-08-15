ed434589ba35666f5309328dd8f731f4fb832a67
Shreyascr5 <shreyas050801@gmail.com>
2023-08-15 15:46:03 +0530

    updated the cart images

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: 'https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s7-1.jpg',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img: 'https://fdn2.gsmarena.com/vv/pics/google/google-pixel-3a-1.jpg',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: 'https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg',
+    amount: 1,
+  },
+];
